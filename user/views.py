1fa9361fc98c45252b5aaee723a5bf0b08525527
KyleXID <assaboy112@gmail.com>
2019-04-30 18:04:23 +0900

    bcrypt를 통한 유저 password 암호화
    bcrypt설치 후, 유저 생성시 password를 암호화하도록 함.

--- user/views.py
+++ user/views.py
@@ -2,15 +2,18 @@ from django.http import JsonResponse, HttpResponse
 from .models import User
 from django.views import View
 import json
+import bcrypt
 
 class UserInfo(View):
 
     def post(self, request):
         new_user = json.loads(request.body)
-        
+        password = bytes(new_user['user_password'], "utf-8")
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
         User(
             user_name = new_user['user_name'],
-            user_password = new_user['user_password'],
+            user_password = hashed_password,
             user_gender = new_user['user_gender']
         ).save()
 

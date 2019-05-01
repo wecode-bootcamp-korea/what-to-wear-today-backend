4ef43a4d69dd627597c80b6bac9d79959e72bc05
KyleXID <assaboy112@gmail.com>
2019-05-01 21:50:54 +0900

    로그인 체크 데코레이터 구현
    해당 유저가 로그인하면 해당 아이디를 token으로 변환하여 front에 보냄.
    데코레이터:
    token형태의 값을 헤더 'Authorization'에서 받음.
    받은 값을 decode하여 유저확인하는 형태로 구현.
    만약 로그인하지 않은 경우에는 해당 엔드포인트에 접근불가.(Token이 없기 때문)

--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 from .views import UserView
 from .views import LoginView
+from .views import InfoView
 
 urlpatterns = [
     path('', UserView.as_view()),
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('info/', InfoView.as_view())
 ]
 
--- user/views.py
+++ user/views.py
@@ -3,6 +3,30 @@ from .models import User
 from django.views import View
 import json
 import bcrypt
+import jwt
+from django.core.exceptions import ObjectDoesNotExist
+
+def login_decorator(f):
+    def wrapper(self, request, *args, **kwargs):
+        access_token = request.headers.get('Authorization', None)
+
+        try:
+            if access_token:
+                decoded = jwt.decode(access_token, 'secret', algorithms=['HS256'])
+                print(decoded)
+                user_id = decoded["user_id"]
+                user = User.objects.get(id=user_id)
+                request.user = user
+
+                return f(self, request, *args, **kwargs)
+            else:
+                return HttpResponse(status=401)
+        except jwt.DecodeError:
+            print(access_token)
+            return HttpResponse(status=401)
+
+    return wrapper
+
 
 class UserView(View):
 
@@ -19,21 +43,32 @@ class UserView(View):
 
         return HttpResponse(status=200)
 
-class LoginView(View):
 
+class InfoView(View):
+
+    @login_decorator
+    def get(self, request):
+        return JsonResponse({
+            'user_name' : request.user.user_name
+        })
+
+class LoginView(View):
+    
     def post(self, request):
         login_user = json.loads(request.body)
 
         try: 
             user = User.objects.get(user_name=login_user['user_name'])
+            encoded_jwt_id = jwt.encode({'user_id' : user.id}, 'secret', algorithm='HS256')
 
             if bcrypt.checkpw(login_user['user_password'].encode("UTF-8"), user.user_password.encode("UTF-8")):
-                return JsonResponse({"Success" : True})
+               # print(encoded_jwt_id.decode("UTF-8"))
+                return JsonResponse({"access_token" : encoded_jwt_id.decode("UTF-8")})
             else:
                 return HttpResponse(status=401)
 
-        except DoesNotExist:
+        except ObjectDoesNotExist:
             return HttpResponse(status=401)
         except Exception as e:
+            print(e)
             return HttpResponse(status=500)
-

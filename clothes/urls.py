588b93d95726f9417a3a44a3e929c764d55a6f79
KyleXID <assaboy112@gmail.com>
2019-05-07 22:01:44 +0900

    탑10리스트 엔드포인트 및 하트리스트 엔드포인트 수정
    기존의 하트리스트 엔드포인트는 img_id만 보내서 추가적으로 img_ref, total_hearts를 보냄.
    탑10리스트의 엔드포인트를 생성함.

--- clothes/urls.py
+++ clothes/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 from .views import HeartView    
-        
+from .views import Top10View
+
 urlpatterns = [
     path('/heart', HeartView.as_view()),
+    path('/top10', Top10View.as_view())
 ]   
 
--- clothes/views.py
+++ clothes/views.py
@@ -1,4 +1,5 @@
 import json
+from operator import itemgetter
 
 from django.views import View
 from django.http import JsonResponse, HttpResponse
@@ -13,7 +14,7 @@ class HeartView(View):
     def post(self, request):
         user     = request.user
         cloth    = json.loads(request.body)
-        cloth_id = cloth['id']
+        cloth_id = cloth['img_id']
         cloth    = Cloth.objects.get(id = cloth_id)
 
         if cloth.hearts.filter(id = user.id).exists():
@@ -28,7 +29,19 @@ class HeartView(View):
     @login_decorator
     def get(self, request):
         user        = request.user
-        hearts_list = list(Cloth.objects.filter(hearts__id=user.id).values('pk'))
-        cloth_id    = [d['pk'] for d in hearts_list]
+        hearts_list = list(Cloth.objects.filter(hearts__id=user.id).values('pk', 'img_ref'))
+        cloth_id    = [{'img_id' : d['pk'], 'img_ref' : d['img_ref'], 'total_heart': Cloth.objects.get(id = d['pk']).total_hearts} for d in hearts_list]
 
-        return JsonResponse({'cloth_id' : list(reversed(cloth_id))})
+        return JsonResponse({'heart_list' : list(reversed(cloth_id))})
+
+
+class Top10View(View):
+
+    def get(self, request):
+        hearts_list       = list(Cloth.objects.all().values('hearts__id').values('pk').distinct())
+        total_hearts_list = [{"img_id" : d['pk'], "total_heart" : Cloth.objects.get(id = d['pk']).total_hearts} for d in hearts_list]
+        data              = sorted(total_hearts_list, key = itemgetter('total_heart'))
+        data.reverse()
+        top10             = data[0 : min(10,len(data))]
+        
+        return JsonResponse({'top10_list' : top10})
